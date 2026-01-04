1ac555d0aca73cf4336964f56d575175ac7181c2
Dereck2102 <dereckmas1236712@hotmail.com>
2026-01-03 22:15:57 -0500

    feat(inventory): connect to postgres and enable auto-schema sync

--- apps/inventory-service/src/app/app.module.ts
+++ apps/inventory-service/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Asset } from './entities/asset.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Docker expone el puerto 5432 en localhost
+      port: 5432,
+      username: 'sima',      // Definido en docker-compose.yml
+      password: 'password123', // Definido en docker-compose.yml
+      database: 'sima_core',   // Definido en docker-compose.yml
+      entities: [Asset], // ¡Aquí cargamos tu entidad de Activos!
+      synchronize: true, // AUTO-SCHEMA: Esto creará la tabla automáticamente (Solo dev)
+    }),
+    TypeOrmModule.forFeature([Asset]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
