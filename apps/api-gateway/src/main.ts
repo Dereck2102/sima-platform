a4f17c4a1d398b8185f46ff03db870cff24f0dae
Dereck2102 <dereckmas1236712@hotmail.com>
2026-01-05 01:41:18 -0500

    feat(gateway): implement reverse proxy to inventory service on port 3000

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,21 +1,33 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.use(
+    '/api/assets',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/': '/api/assets/', // Re-agrega el prefijo al inicio
+      },
+    })
+  );
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+
+  Logger.log(`API Gateway running on: http://localhost:${port}`);
+  Logger.log(`Route Active: /api/assets -> http://localhost:3001/api/assets`);
 }
 
 bootstrap();
